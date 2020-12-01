5ed391a9979af8b256e54ad89c852a620084e56c
Navoznov, Ivan E <Navoznov.IE@parma.ru>
2020-12-01 13:27:23 +0300

    day02 рефакторинг первой части

--- Day02/Program.py
+++ Day02/Program.py
@@ -4,25 +4,28 @@ lines = open('Day02\input.txt').read().split(',')
 numbers = [int(line) for line in lines]
 codes = numbers
 
-codes[1] = 12
-codes[2] = 2
 
-counter = 0
-while True:
-    code = codes[counter]
+def getResult(codes, noun, verb):
+    counter = 0
+    codes[1] = noun
+    codes[2] = verb
+    while True:
+        code = codes[counter]
 
-    if code == 99:
-        break
+        if code == 99:
+            return codes[0]
 
-    number1 = codes[codes[counter + 1]]
-    number2 = codes[codes[counter + 2]]
-    if code == 1:
-        result = number1 + number2
-    elif code == 2:
-        result = number1 * number2
+        number1 = codes[codes[counter + 1]]
+        number2 = codes[codes[counter + 2]]
+        if code == 1:
+            result = number1 + number2
+        elif code == 2:
+            result = number1 * number2
 
-    targetIndex = codes[counter + 3]
-    codes[targetIndex] = result
-    counter = counter + 4
+        targetIndex = codes[counter + 3]
+        codes[targetIndex] = result
+        counter = counter + 4
 
-print(codes[0])
+
+part1 = getResult(codes, 12, 2)
+print(part1)
